f94c19e1b79145291018fcf367e2405a2aac1dd0
Student <student@lan.itstep.kiev.ua>
2021-10-30 10:31:58 +0300

    images

---
+++ PersonalOrganizer/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PersonalOrganizer.Models;
+namespace PersonalOrganizer.ViewModels
+{
+    class CategoryViewModel :INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
